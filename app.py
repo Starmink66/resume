6f974ff4c13e24dc575f032bc155fa4bc3da7704
Starmink66 <117124823+Starmink66@users.noreply.github.com>
2026-03-11 12:02:13 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+if __name__ == '__main__':
+    app.run(debug = True)
